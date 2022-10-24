707915ec0f07534a6393254dd19d42a24de90f49
hamzeh Fatfat <hamze.fatfat@gmail.com>
2022-10-24 03:12:40 +0300

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Hamzeh Fatfat on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
